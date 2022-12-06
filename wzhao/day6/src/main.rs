1201324064afd9014261d9956b7d5255daf36ec3
William Zhao <wzhao@epicblockchain.io>
2022-12-06 11:13:38 -0500

    day 6 idiomatic solution

--- wzhao/day6/src/main.rs
+++ wzhao/day6/src/main.rs
@@ -1,29 +1,16 @@
-const LENGTH: usize = 14;
-
+const LENGTH: usize = 4;
 fn main() {
-    let text = include_str!("input.txt").lines().collect::<Vec<_>>();
-
-    for i in text {
-        let chars: Vec<char> = i.chars().collect();
-        for x in LENGTH..chars.len() {
-            let mut count = 0;
-            let mut seen: Vec<char> = vec![];
-            for y in x-LENGTH..x {
-                if !seen.contains(&chars[y]) {
-                    count += 1;
-                    seen.push(chars[y]);
-                }
-                else {
-                    break;
-                }
-                if count >= LENGTH {
-                    println!("{}", x);
-                    break;
-                }
-            }
-            if count >= LENGTH {
+    let text: Vec<char> = include_str!("input.txt").lines().collect::<Vec<_>>()[0].chars().collect();
+    for i in 0..text.len() {
+        let mut seen = std::collections::BTreeSet::new();
+        for j in i..i+LENGTH {
+            if !seen.insert(text[j]) {
                 break;
             }
+        }
+        if seen.len() == LENGTH {
+            println!("{}", i+LENGTH);
+            break;
         }
     }
 }
